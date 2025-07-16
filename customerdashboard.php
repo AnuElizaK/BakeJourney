134ec2d0201683b8372ac7c04547f9a0876910e2
Acsah Rojan <acsah.rojan@gmail.com>
2025-07-16 18:13:05 +0530

    .

--- customerdashboard.php
+++ customerdashboard.php
@@ -4,6 +4,11 @@ if (!isset($_SESSION['email']) || $_SESSION['role'] !== 'customer') {
     header("Location: index.php"); // Redirect to login if not authorized
     exit();
 }
+// Prevent back after logout
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Expires: Sat, 1 Jan 2000 00:00:00 GMT");
+header("Pragma: no-cache");
+
  ?>
 <!DOCTYPE html>
 <html lang="en">
